1b6e84c3881301da3a32924e1f873695fbce1b16
Erica Wright and Michaela Davis <erica@>
2017-02-07 15:49:59 -0800

    move html to views folder temmplates and update app.php with twig

---
+++ app/app.php
@@ -0,0 +1,35 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Tasks.php";
+
+    session_start();
+
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
+
+    $app = new Silex\Application();
+    
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Tasks::getAll()));
+    });
+
+    $app->post("/tasks", function() use ($app) {
+        $task = new Tasks($_POST['description']);
+        $task->save();
+        return $app['twig']->render('create_tasks.html.twig', array('newtask' => $task));
+    });
+
+    $app->post("/delete_tasks", function() use ($app) {
+        Tasks::deleteAll();
+        return $app['twig']->render('delete_tasks.html.twig');
+    });
+
+    return $app;
+?>
