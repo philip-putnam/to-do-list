115b8aa1812aa6dd6e09272f8ddb782f40986caf
philip putnam <putnamp989@gmail.com>
2017-02-19 14:12:23 -0800

    add twig, link Category class to app.php

--- app/app.php
+++ app/app.php
@@ -2,6 +2,7 @@
     date_default_timezone_set("America/Los_Angeles");
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
 
     // session_start();
     //
@@ -21,20 +22,37 @@
     ));
 
     $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
 
+    $app->get("/tasks", function() use ($app) {
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
 
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
     });
 
     $app->post("/tasks", function() use ($app) {
         $task = new Task($_POST['description']);
         $task->save();
-        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
     $app->post("/delete_tasks", function() use ($app) {
-      Task::deleteAll();
-      return $app['twig']->render('delete_tasks.html.twig');
+        Task::deleteAll();
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.html.twig');
     });
 
     return $app;
